86e72a5fd05f85a07aaf9fbd0120d785fb9e2ca3
kpawel-29 <pakadlubowski@gmail.com>
2015-05-29 17:58:37 +0200

    init commit

---
+++ app/scripts/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularProjectApp
+ * @description
+ * # angularProjectApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularProjectApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
